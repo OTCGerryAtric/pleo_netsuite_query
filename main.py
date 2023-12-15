431470118cb702e0154b8646a5a70a948372dc90
Obee17 <gerrypidgeon@outlook.com>
2023-12-15 16:44:39 +0000

    Log Added and Requirements Updated

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd
-import openpyxl
 import os
+import logging
 
-# Import Functions
-from accounting_period import accounting_period_function
-from cash_balance_eur import cash_balance_function
-from tx_detail_eur import tx_detail_function
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
-# Call the function to create and process the DataFrames and assign them to variables
-acc_period = accounting_period_function()
-tx_detail = tx_detail_function()
-cash_balance = cash_balance_function()
+try:
+    # Import Functions
+    from accounting_period import accounting_period_function
+    from cash_balance_eur import cash_balance_function
+    from tx_detail_eur import tx_detail_function
+
+    # Call the function to create and process the DataFrames and assign them to variables
+    acc_period = accounting_period_function()
+    tx_detail = tx_detail_function()
+    cash_balance = cash_balance_function()
+except Exception as e:
+    logging.exception("An unexpected error occurred")
